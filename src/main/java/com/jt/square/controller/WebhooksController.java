ca0b8a47001d3d0279238f7c5b62cd6e2f9b9ad6
渡部保 <tamotsu917@yahoo.co.jp>
2018-07-08 21:15:51 +0900

    modify

--- src/main/java/com/jt/square/controller/WebhooksController.java
+++ src/main/java/com/jt/square/controller/WebhooksController.java
@@ -1,7 +1,12 @@
 package com.jt.square.controller;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,9 +33,25 @@ public class WebhooksController {
 
 	@RequestMapping(value = "/square/api/webhooks", method = RequestMethod.POST)
 	public ResponseEntity<String> webhooks(@RequestHeader("X-Square-Signature") String squareSignature,
-			@RequestBody String body) throws JsonParseException, JsonMappingException, IOException {
+			@RequestBody String body) throws JsonParseException, JsonMappingException, IOException, InvalidKeyException,
+			NoSuchAlgorithmException {
 
 		System.out.println(squareSignature);
+
+		final String WEBHOOK_URL = "https://square-sample.herokuapp.com/square/api/webhooks";
+		final String SIGNATURE_KEY = "JOXgbcr-X_Pg0sK69NnRdw";
+
+		String stringToSign = WEBHOOK_URL + body;
+
+		String algo = "HMacSHA1";
+		final SecretKeySpec keySpec = new SecretKeySpec(SIGNATURE_KEY.getBytes(), algo);
+		final Mac mac = Mac.getInstance(algo);
+		mac.init(keySpec);
+		final byte[] signBytes = mac.doFinal(stringToSign.getBytes());
+		for (byte signByte : signBytes) {
+			System.out.printf("%02x", signByte & 0xff);
+		}
+		System.out.print("\n");
 
 		ObjectMapper mapper = new ObjectMapper();
 		WebhooksDto dto = mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE).readValue(body,
